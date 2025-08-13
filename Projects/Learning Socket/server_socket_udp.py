2ce3f8bba8fa614355376fa0606875c38abe132a
Tomislav Simovic <tomislav.simovic@yahoo.com>
2025-08-13 09:29:27 +0200

    delete the server.listen() func. because udp dont need it

--- Projects/Learning Socket/server_socket_udp.py
+++ Projects/Learning Socket/server_socket_udp.py
@@ -4,4 +4,4 @@ socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 socket_server.bind((socket.gethostbyname(socket.gethostname()), 12345))
 
-socket_server.listen()
+message, address = socket_server.recvfrom(1024)
