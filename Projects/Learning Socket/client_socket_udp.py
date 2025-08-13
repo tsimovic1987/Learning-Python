cef3577d471e0f1997a93d6f49d53d9bfc9fe79b
Tomislav Simovic <tomislav.simovic@yahoo.com>
2025-08-13 09:37:00 +0200

    change bug: a tuple was incorrectly assigned

--- Projects/Learning Socket/client_socket_udp.py
+++ Projects/Learning Socket/client_socket_udp.py
@@ -2,5 +2,5 @@ import socket
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-socket_client.sendto("Hello from the Server".encode("utf-8")),
-(socket.gethostbyname(socket.gethostname))
+socket_client.sendto("Hello from the Server".encode("utf-8"),
+                     socket.gethostbyname(socket.gethostname(), 12345))
