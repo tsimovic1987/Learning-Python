1b3479ad84b372d5b4167499345da13659ef4c07
Tomislav <tomislav.simovic@yahoo.com>
2025-08-17 17:56:47 +0200

    add more code to class Bobble

--- Projects/bobble/bobble.py
+++ Projects/bobble/bobble.py
@@ -16,11 +16,17 @@ class Bobble:
         return 3 + len(self.kwargs)
 
     def __eq__(self, other):
-        pass
+        if not isinstance(other, Bobble):
+            return NotImplemented
+        return (self.path == other.path and
+                self.argument1 == other.argument1 and
+                self.argument2 == other.argument2)
+
+    def __hash__(self):
+        return hash((self.path, self.argument1, self.argument2))
 
     def create_command(self):
         pass
-    #   
 
 comm1 = Bobble("pfad", "add", "table", user="root")
 print(len(comm1))
