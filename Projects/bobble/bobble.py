6f81f4228d60a71bc9ad448f0c8efb9e7ddb39b4
Tomislav Simovic <tomislav.simovic@yahoo.com>
2025-08-17 21:26:19 +0200

    trying new function and just want to test something

--- Projects/bobble/bobble.py
+++ Projects/bobble/bobble.py
@@ -1,4 +1,8 @@
-class Bobble:
+import argparse
+import subprocess
+
+
+class BobbleCommands:
     def __init__(self, path, argument1, argumenmt2, **kwargs):
         self.path = path
         self.argument1 = argument1
@@ -25,9 +29,20 @@ class Bobble:
     def __hash__(self):
         return hash((self.path, self.argument1, self.argument2))
 
-    def create_command(self):
-        pass
+open_epic = BobbleCommands("pfad", "open", "as", user="root")
+print(len(open_epic))
+print(open_epic)
 
-comm1 = Bobble("pfad", "add", "table", user="root")
-print(len(comm1))
-print(comm1)
+
+def create_command(keyword, file):
+    parser = argparse.ArgumentParser(description="Bobble - I love CLI")
+    parser.add_argument(keyword, help="Open Command for File/Exe")
+    parser.add_argument(file, help="File name")
+    args = parser.parse_args()
+    if args.keyword == "open":
+        try:
+            subprocess.run([args.file])
+        except FileNotFoundError:
+            print(f"File: {args.file} doesnt exists")
+
+create_command("open", "epic.exe")
